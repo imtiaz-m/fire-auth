225baf84898709970e95578ed74b41ca6840f308
imtiaz-m <61225374+imtiaz-m@users.noreply.github.com>
2020-05-02 00:31:23 +0600

    autentication done

---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAgWSKvmZbsFVTO7OyQTX_lStb3AfDwDnE",
+    authDomain: "ema-john-simple-3eedd.firebaseapp.com",
+    databaseURL: "https://ema-john-simple-3eedd.firebaseio.com",
+    projectId: "ema-john-simple-3eedd",
+    storageBucket: "ema-john-simple-3eedd.appspot.com",
+    messagingSenderId: "335591485018",
+    appId: "1:335591485018:web:5042c2857bf236901f7263"
+  };
+
+  export default firebaseConfig ;
